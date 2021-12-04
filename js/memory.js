2cf9f9315cca2f89b0cf1b33dc8e7dc72085579f
jeru27 <jrumora@gmail.com>
2021-12-03 18:17:15 -0800

    commit

--- js/memory.js
+++ js/memory.js
@@ -2,20 +2,44 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
     // ... write your code here
+    for (let i = 0; i < this.cards.length; i++) {
+      let j = Math.random(this.cards.length)
+      console.log(this.cards)
+      if (j !== i) {
+        this.cards[i] = this.cards[j]
+        return this.cards[i]
+      }
+    }
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.pairsClicked += 1
+    if (card1 === card2) {
+      this.pairsGuessed += 1
+      return true 
+    } else {
+      return false
+    }
   }
 
   checkIfFinished() {
     // ... write your code here
+    if (this.pairsGuessed ===  this.cards.length / 2) {
+      return true
+    } else {
+      return false
+    }
   }
 }
+
 
 // The following is required for automated testing. Please, ignore it.
 if (typeof module !== 'undefined') module.exports = MemoryGame;
